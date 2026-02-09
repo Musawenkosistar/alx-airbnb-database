08db1d4e3c4758f2a0d36d3399fb90e2b8af249d
Musawenkosistar <musawenkosi2504@gmail.com>
2026-02-09 05:54:49 -0800

    ff

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,44 +1,15 @@
 SELECT 
-    b.booking_id,
-    b.check_in,
-    b.check_out,
-    u.user_id,
-    u.username,
-    u.email
-FROM 
-    bookings b
-INNER JOIN 
-    users u
-ON 
-    b.user_id = u.user_id;
-
-SELECT
     p.property_id,
     p.title,
     p.location,
     r.review_id,
     r.rating,
     r.comment
-FROM
+FROM 
     properties p
-LEFT JOIN
+LEFT JOIN 
     reviews r
-ON
-    p.property_id = r.property_id;
-
-SELECT
-    u.user_id,
-    u.username,
-    u.email,
-    b.booking_id,
-    b.check_in,
-    b.check_out,
-    b.property_id
-FROM
-    users u
-FULL OUTER JOIN
-    bookings b
-ON
-    u.user_id = b.user_id;
-
-
+ON 
+    p.property_id = r.property_id
+ORDER BY 
+    p.property_id;
